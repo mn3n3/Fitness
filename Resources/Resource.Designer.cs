1f0687f53f07aacb3a4c0b5ad85b90fc749f7ac2
Mohammed Awad <52838423+MohdAwad@users.noreply.github.com>
2020-09-05 22:59:32 +0300

    Push Test Mohammed Awad

--- Resources/Resource.Designer.cs
+++ Resources/Resource.Designer.cs
@@ -160,6 +160,15 @@ namespace Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Save.
+        /// </summary>
+        public static string Save {
+            get {
+                return ResourceManager.GetString("Save", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to This Account Has Been Locked.
         /// </summary>
         public static string ThisAccountHasBeenLocked {
