c5063454f77f11f6f4afa689c9d47d32f156d20f
n.alsharif@catnipsoft.com <n.alsharif@catnipsoft.com>
2020-09-08 15:46:34 +0300

    Push 0800920 NS 2

---
+++ Fitness/Migrations/202009081245531_Nagham080920.cs
@@ -0,0 +1,16 @@
+namespace Fitness.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Nagham080920 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
